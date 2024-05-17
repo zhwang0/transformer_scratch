2a7f7a88fc3c0e05b208e69fb2b31d8902a4cc80
zhwang0 <zhwang1@umd.edu>
2024-05-17 12:16:25 -0400

    Make comments on positional encoding.

--- model.py
+++ model.py
@@ -15,27 +15,29 @@ class InputEmbedding(nn.Module):
   
 class PositionalEncoding(nn.Module):
   
-  def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+  def __init__(self, d_model: int, max_seq_len: int, dropout: float) -> None: # return None from __init__ method, to enhance understanding and clarity
     super().__init__()
     self.d_model = d_model
-    self.seq_len = seq_len
+    self.max_seq_len = max_seq_len
     self.dropout = nn.Dropout(dropout)
     
-    # create a matrix of shape (seq_len, d_model)
-    pe = torch.zeros(seq_len, d_model)
-    # create a vector of shape (seq_len, 1)
-    position = torch.arange(0, seq_len, dtype=torch.float32).unsqueeze(1)
-    div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(torch.log(torch.tensor(10000.0)) / d_model))
-    # apply the sin to even indices in the vector
+    pe = torch.zeros(max_seq_len, d_model)
+    position = torch.arange(0, max_seq_len, dtype=torch.float32).unsqueeze(1) # add 1 in the dimension index 1 to make it (max_seq_len, 1)
+    '''
+    Why the div_term is computed in this way? different from the original paper?
+      - Numerical Stability: Direct computation of very large or very small powers can lead to numerical instability due to floating-point precision limitations.
+      - Efficiency: Exponentiation with large bases and exponents can be computationally expensive.
+    '''
+    div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float32) * -(torch.log(torch.tensor(10000.0)) / d_model)) 
     pe[:, 0::2] = torch.sin(position * div_term)
     pe[:, 1::2] = torch.cos(position * div_term)
     
-    pe = pe.unsqueeze(0) # add a batch dimension (1, seq_len, d_model)
+    pe = pe.unsqueeze(0) # add a batch dimension (1, max_seq_len, d_model)
     
     self.register_buffer('pe', pe) # register the positional encoding buffer, so that it will be saved along with the model
   
   def forward(self, x):
-    x = x + self.pe[:, :x.size(1)].requires_grad_(False) # no gradient tracking
+    x = x + self.pe[:, :x.size(1)].requires_grad_(False) # x.size(1) is the current seq_len, which is less than max_seq_len initialized # no gradient tracking
     return self.dropout(x)
   
     
