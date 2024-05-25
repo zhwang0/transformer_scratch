9d5070de1cf0e6160eb18ae14bbf417081311207
zhwang0 <zhwang1@umd.edu>
2024-05-24 21:22:17 -0400

    Complete everything except tensorboard usage.

--- model.py
+++ model.py
@@ -196,10 +196,10 @@ class Encoder(nn.Module):
 
 class DecoderBlock(nn.Module): 
   
-  def __init__(self, self_attention_block: MultiHeadAttentionBlock, corss_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None: 
+  def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None: 
     super().__init__()
     self.self_attention_block = self_attention_block
-    self.corss_attention_block = corss_attention_block
+    self.cross_attention_block = cross_attention_block
     self.feed_forward_block = feed_forward_block
     self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
     
@@ -208,7 +208,7 @@ class DecoderBlock(nn.Module):
     # tgt_mask/decoder_mask is used to mask the future tokens from the decoder output
     
     x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask))
-    x = self.residual_connections[1](x, lambda x: self.corss_attention_block(x, encoder_output, encoder_output, src_mask))
+    x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
     x = self.residual_connections[2](x, self.feed_forward_block)
     return x
   
