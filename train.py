795c1740ff15f34ba59faa1c6ad61ba9879e58e7
zhwang0 <zhwang1@umd.edu>
2024-05-24 20:00:46 -0400

    Completed validation function.

--- train.py
+++ train.py
@@ -14,9 +14,108 @@ from tokenizers.pre_tokenizers import Whitespace # A pre-tokenizer that splits t
 
 from torch.utils.tensorboard import SummaryWriter
 
-from pathlib import Path
-from tqdm import tqdm 
 import warnings
+from tqdm import tqdm 
+from pathlib import Path
+
+
+def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device): 
+  sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+  eos_idx = tokenizer_tgt.token_to_id('[ESO]')
+  
+  # precompute the encoder output and reuse itfor every token we get from the decoder
+  encoder_output = model.encode(source, source_mask)
+
+  # initialize the decoder input with the SOS token
+  decoder_input = torch.empty(1,1).fill_(sos_idx).type_as(source).to(device)
+  while True: 
+    if decoder_input.size(1) == max_len: 
+      break
+    
+    # build mask for target (decoder input)
+    decoder_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(device)
+    
+    # calculate the output of the decoder
+    out = model.decode(decoder_input, encoder_output, source_mask.unsqueeze(0), decoder_mask)
+    
+    # get the next token 
+    prob = model.project(out[:,-1])
+    
+    # select the token with the max probability (greedy decoding)
+    _, next_word = torch.max(prob, dim=1)
+    # decoder_input = torch.cat([decoder_input, next_word.unsqueeze(-1)], dim=-1) # more efficient for batch operations 
+    decoder_input = torch.cat([decoder_input, torch.empty(1,1).type_as(source).fill_(next_word.item()).to(device)], dim=-1) # more suitable for single-item prediction
+    
+    if next_word == eos_idx: 
+      break
+  
+  return decoder_input.squeeze(0) # remove the batch dimension
+
+
+def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, global_state, writer, num_examples=2): 
+  model.eval()
+  count = 0 
+  
+  source_texts = []
+  expected = []
+  predicted = []
+  
+  # sizze of the control window (just use a default value)
+  try: 
+    with os.popen('stty size', 'r') as console_size: 
+      console_width = int(console_size.read().split()[1])
+  except:
+    console_width = 80
+    
+  
+  with torch.no_grad():
+    for batch in validation_ds: 
+      count += 1
+      encoder_input = batch['encoder_input'].to(device) # (b, seq_len)
+      encoder_mask = batch['encoder_mask'].to(device) # (b, 1, 1, seq_len)
+      decoder_input = batch['decoder_input'].to(device)
+      decoder_mask = batch['decoder_mask'].to(device)
+      
+      assert encoder_input.shape[0] == 1, 'Batch size for validation should be 1'
+      
+      model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
+      
+      source_text = batch['src_text'][0]
+      target_text = batch['tgt_text'][0]
+      model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+      
+      source_texts.append(source_text)
+      expected.append(target_text)
+      predicted.append(model_out_text)
+      
+      print_msg('-'*console_width) # tqdm is not suitable for print()
+      print_msg(f'Source: {source_text}')
+      print_msg(f'Target: {target_text}')
+      print_msg(f'Predicted: {model_out_text}')
+      
+      if count == num_examples: 
+        break
+      
+  if writer: 
+    # evaluate the cahracter error rate 
+    # compute the char error rate
+    metric = torchmetircs.CharErrorRate()
+    cer = mecric(expected, predicted)
+    writer.add_scalar('Validation cer', cer, global_state)
+    writer.flush()
+    
+    # compute the word error rate
+    metric = torchmetrics.WordErrorRate()
+    wer = metric(expected, predicted)
+    writer.add_scalar('Validation wer', wer, global_state)
+    writer.flush()
+    
+    # compute the BLEU score
+    metric = torchmetrics.BLEU()
+    bleu = metric(expected, predicted)
+    writer.add_scalar('Validation bleu', bleu, global_state)
+    writer.flush()
+
 
 
 def get_all_sentences(ds, lang):
